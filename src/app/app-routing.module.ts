ad7343079e08bfadf9bc682ad34248db04ed0aba
zhpinkman <zhivarsourati@gmail.com>
2019-11-24 15:56:19 +0330

    observable and some other aspect of using ngfor directive added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HeroesComponent } from "./heroes/heroes.component";
+import { RoutingComponent } from "./routing/routing.component";
+import { TestingComponent } from "./testing/testing.component";
 
 const routes: Routes = [
   {
     path: "heroes",
     component: HeroesComponent
+  },
+  {
+    path: "routing/:id",
+    component: RoutingComponent
+  },
+  {
+    path: "test",
+    component: TestingComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,21 @@ import { AppComponent } from "./app.component";
 import { HeroesComponent } from "./heroes/heroes.component";
 import { FormsModule } from "@angular/forms";
 import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
-import { MessagesComponent } from './messages/messages.component';
+import { MessagesComponent } from "./messages/messages.component";
+import { RoutingComponent } from "./routing/routing.component";
+import { TestingComponent } from "./testing/testing.component";
+import { TestPipe } from "./testing/testPipe";
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    RoutingComponent,
+    TestingComponent,
+    TestPipe
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
